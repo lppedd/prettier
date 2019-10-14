15fa2f875e878367c97094927ad40dec00b44f6c
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2019-10-15 02:55:06 +0300

    initial support for TSDeclareMethod

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -1451,6 +1451,7 @@ function printPathNoParens(path, options, print, args) {
     case "ClassPrivateMethod":
     case "MethodDefinition":
     case "TSAbstractMethodDefinition":
+    case "TSDeclareMethod":
       if (n.decorators && n.decorators.length !== 0) {
         parts.push(printDecorators(path, options, print));
       }
@@ -1460,7 +1461,7 @@ function printPathNoParens(path, options, print, args) {
       if (n.static) {
         parts.push("static ");
       }
-      if (n.type === "TSAbstractMethodDefinition") {
+      if (n.type === "TSAbstractMethodDefinition" || n.abstract) {
         parts.push("abstract ");
       }
 
